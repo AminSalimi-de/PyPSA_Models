200497392f4247326504e602329a9de0a84e0c9b
AminSalimi <amin.salimi.de@gmail.com>
2024-12-22 23:08:43 +0100

    Added Global Constraint for CO2 Emission

--- Models/SimpleElectricityMarket.py
+++ Models/SimpleElectricityMarket.py
@@ -14,7 +14,7 @@ emissions = dict(coal=0.34, gas=0.2, oil=0.26, hydro=0, wind=0)
 #       Power Plants Capacities (MW)
 power_plants = {
     "SA": {"coal": 35000, "wind": 3000, "gas": 8000, "oil": 2000},
-    "MZ": {"hydro": 1200}
+    "MZ": {"hydro": 1200},
 }
 
 #       MW
@@ -56,38 +56,22 @@ for tech, capacity in power_plants["SA"].items():
         bus="SA",
         carrier=tech,
         efficiency=efficiency.get(tech, 1),
-        marginal_cost=fuel_cost.get(tech, 0)/efficiency.get(tech, 1),
-        p_nom=capacity
+        marginal_cost=fuel_cost.get(tech, 0) / efficiency.get(tech, 1),
+        p_nom=capacity,
     )
 print(n.generators)
 
 #       Demands:
 n.add(
-    "Load",
-    "SA electricity demand",
-    bus="SA",
-    carrier="electricity",
-    p_set=loads["SA"]
+    "Load", "SA electricity demand", bus="SA", carrier="electricity", p_set=loads["SA"]
 )
 n.add(
-    "Load",
-    "MZ electricity demand",
-    bus="MZ",
-    carrier="electricity",
-    p_set=loads["MZ"]
+    "Load", "MZ electricity demand", bus="MZ", carrier="electricity", p_set=loads["MZ"]
 )
 print(n.loads)
 
 #       Lines:
-n.add(
-    "Line",
-    "SA-MZ",
-    bus0="SA",
-    bus1="MZ",
-    s_nom=500,
-    x=1,
-    r=1
-)
+n.add("Line", "SA-MZ", bus0="SA", bus1="MZ", s_nom=500, x=1, r=1)
 print(n.lines)
 
 #       Optimize:
@@ -95,25 +79,44 @@ n.optimize(solver_name="highs")
 
 #       Results:
 print(n.generators_t.p)
-print(50*"-")
+print(50 * "-")
 print(n.lines_t.p0)
 print(n.lines_t.p1)
-print(50*"-")
+print(50 * "-")
 print(n.buses_t.marginal_price)
 
 #       Plot:
-n.plot(
-    margin=1,
-    bus_sizes=2,
-    bus_colors="orange",
-    bus_alpha=0.7,
-    color_geomap=True,
-    line_colors="orchid",
-    line_widths=3,
-    title="Test",
-); plt.show()
+# n.plot(
+#     margin=1,
+#     bus_sizes=2,
+#     bus_colors="orange",
+#     bus_alpha=0.7,
+#     color_geomap=True,
+#     line_colors="orchid",
+#     line_widths=3,
+#     title="Test",
+# ); plt.show()
 
 s = n.generators_t.p.loc["now"].groupby([n.generators.bus, n.generators.carrier]).sum()
 print(s)
+# n.plot(margin=1, bus_sizes=s / 1e4); plt.show()
 
-n.plot(margin=1, bus_sizes=s / 1e4); plt.show()
+#       Global Constraint for Emission Limit:
+generators_emissions = (
+    n.generators_t.p
+    / n.generators.efficiency
+    * n.generators.carrier.map(n.carriers.co2_emissions)
+)
+total_emissions = generators_emissions.sum().sum()
+print(f"total emissions = {total_emissions}")
+
+n.add(
+    "GlobalConstraint",
+    "emission_limit",
+    carrier_attribute="co2_emissions",
+    sense="<=",
+    constant=total_emissions * 0.9,
+)
+
+n.optimize()
+print(n.generators_t.p)
